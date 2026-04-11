bf7e07aa29025e67a69e205a88fcc1012102e4d7
Cham Manganti <chammanganti@gmail.com>
2026-04-11 17:30:20 +0800

    feat: add an endpoint to test sentry

--- internal/handler/ready.go
+++ internal/handler/ready.go
@@ -1,7 +1,26 @@
 package handler
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/getsentry/sentry-go"
+)
 
 func Ready(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+}
+
+func ToPanic(w http.ResponseWriter, r *http.Request) {
+	span := sentry.StartSpan(r.Context(), "ready.to_panic")
+	defer span.Finish()
+
+	magicText := r.PathValue("magic_text")
+	if magicText == os.Getenv("SENTRY_PANIC_MAGIC_TEXT") {
+		panic("Uh oh!")
+	}
+
+	slog.Warn("safe from panic, invalid magic text")
+	http.Error(w, "invalid magic text", http.StatusBadRequest)
 }
